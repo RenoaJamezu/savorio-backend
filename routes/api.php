cc75859c9852ea338843c69a6b96154067582769
Kurasao <jamerolenor@gmail.com>
2025-11-23 23:28:04 +0800

    Refactor API routes for improved organization; separate public and protected routes, and add statistics endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,26 +2,30 @@
 
 use App\Http\Controllers\api\v1\AuthController;
 use App\Http\Controllers\api\v1\RecipeController;
+use App\Models\Recipe;
 use Illuminate\Support\Facades\Route;
 
-// Public routes
-Route::prefix('v1')->group(function () {
+// public routes
 
-    // Authentication
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+// authentication
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-    // Public Recipes
-    Route::get('recipes', [RecipeController::class, 'index']);
+// public Recipes
+Route::get('v1/recipes', [RecipeController::class, 'index']);
+Route::get('v1/statistics', function() {
+    return response()->json([
+        'total_recipes' => Recipe::count(),
+    ]);
 });
 
-// Protected routes (requires authentication)
+// logout
+Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
+
+// protected routes (requires authentication)
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
 
-    // Authentication
-    Route::post('logout', [AuthController::class, 'logout']);
-
-    // Recipe CRUD
+    // recipe CRUD
     Route::apiResource('recipes', RecipeController::class)
         ->only(['store', 'show', 'update', 'destroy']);
 });
